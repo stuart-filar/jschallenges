6b516e363b4d16dc13dff36be8523180829879f3
Stuart Filar <stuart.filar@gmail.com>
2016-09-13 13:33:27 -0400

    challenge two, first problem complete

--- challenge_2/challenge2.js
+++ challenge_2/challenge2.js
@@ -1,5 +1,13 @@
 $(function(){
 
-	//all your code should go here
+	$('.panel h1').on('click', function(){
+		if($(this).attr('data-click-state') == 1) {
+			$(this).attr('data-click-state', 0)
+			$(this).css('color', 'black')
+		} else {
+			$(this).attr('data-click-state', 1)
+			$(this).css('color', 'red')
+		}
+	});
 
-})
+});
