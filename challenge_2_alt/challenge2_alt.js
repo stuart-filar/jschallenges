83838a3f0844b5bf94623bcd00c45ee5541b917f
Stuart Filar <stuart.filar@gmail.com>
2016-09-14 13:39:57 -0400

    final commit, 'finished' for the time being, 1:40p, 2016-09-14

--- challenge_2_alt/challenge2_alt.js
+++ challenge_2_alt/challenge2_alt.js
@@ -27,11 +27,82 @@
  * - play a sound when someone clicks the "Shoot" button. You'll need to read about the <audio> element
  *   and how to use it in JS. You will also need to download a sound bite
  */
+ 	// body styling
+ 	$('body')
+ 		.css('font-family', 'Helvetica')
+ 		.css('text-align', 'center')
+ 		.css('background', 'linear-gradient(to right, blue, white, gold)')
+
+ 	// button styling
+ 	$('button')
+ 		.css('font-size', '16px')
+ 		.css('color', 'white')
+ 		.css('background', 'linear-gradient(to right, green, green, green, green, white)')
+ 		.css('padding', '5px')
+ 		.css('font-weight', 'bold');
+
+ 	// reset button styling
+ 	$('button:first')
+ 		.css('background', 'linear-gradient(to right, red, red, red, red, white)')
+ 		.css('margin-bottom', '20px')
+ 		.css('margin-top', '10px');
+
+ 	// team heading styling
+ 	$('h2:nth-child(1)')
+ 		.css('color', 'blue')
+ 		.css('border', '1px solid black')
+ 		.css('background', 'linear-gradient(to right, black, black, black, grey)')
+ 		.css('border-radius', '5px')
+ 		.css('width', '200px')
+ 		.css('margin-left', '20px');
+
+ 	// replaces 'Team 1' with 'Washington Capitals'
+ 	$('h2:nth-child(1)').each(function() {
+    	var text = $(this).text();
+    	$(this).text(text.replace('Team 1', 'Washington Capitals')); 
+	});
+
+ 	// team 2 heading styling
+ 	$('h2:last')
+ 		.css('color', 'gold');
+
+ 	// replaces 'Team 2' with 'Pittsburgh Penguins'
+ 	$('h2:last').each(function() {
+    	var text = $(this).text();
+    	$(this).text(text.replace('Team 2', 'Pittsburgh Penguins')); 
+	});
+
+	$('h1').text('Welcome to Hockey Game Number One!')
+		.css('font-size', '65px');
+
  	// washington capitals logo
- 	$('.hits:first').prepend('<img src="caps.png"/><br><br>');
+ 	$('.hits:first')
+ 		.prepend('<img src="assets/caps.png"/><br>')
+ 		.css('mix-blend-mode', 'multiply');
 
  	// pittsburgh penguins logo
- 	$('.hits:last').prepend('<img src="pens.png"/><br><br>');
+ 	$('.hits:last')
+ 		.prepend('<img src="assets/pens.png"/><br>')
+ 		.css('mix-blend-mode', 'multiply');
+
+ 	// styling for 'shots taken' & 'goals'
+ 	$('.hits')
+ 		.css('font-weight', 'bold')
+ 		.css('font-size', '30px');
+
+ 	// syling for 'number of resets'
+ 	$('div div')
+ 		.css('font-weight', 'bold');
+
+ 	$('h2:first')
+ 		.hide()
+ 		.css('font-size', '65px')
+ 		.css('margin-bottom', '0px')
+ 		.css('color', 'red')
+ 		.each(function() {
+    		var text = $(this).text();
+			$(this).text(text.replace('...the game!', 'GOAL!!!!!')); 
+		});
 
  	// math randomizer to determine the number associated with the shit
  	function shot() {
@@ -46,9 +117,14 @@
    		//changes background color based on if it was a goal or not
    		if (shot() > .85) {
    			$('#teamone-numhits').html(function(i, val) { return +val+1 });
-   			$('body').css('background-color', 'blue');
+   			$('body').css('background', 'linear-gradient(to right, blue, blue, blue, blue, white)');
+   			var obj = document.createElement("audio");
+       		obj.src= 'assets/foghorn.mp3';
+       		obj.play();
+       		$('h2:first').show();
    		} else {
-   			$('body').css('background-color', 'white');
+   			$('body').css('background', 'linear-gradient(to right, blue, white, gold)');
+   			$('h2:first').hide();
    		}
 	});
 
@@ -60,9 +136,14 @@
    		//changes background color based on if it was a goal or not
    		if (shot() > .85) {
    			$('#teamtwo-numhits').html(function(i, val) { return +val+1 });
-   			$('body').css('background-color', 'yellow');
+   			$('body').css('background', 'linear-gradient(to left, gold, gold, gold, gold, white)');
+   			var obj = document.createElement("audio");
+       		obj.src= 'assets/foghorn.mp3';
+       		obj.play();
+   			$('h2:first').show();
    		} else {
-   			$('body').css('background-color', 'white');
+   			$('body').css('background', 'linear-gradient(to right, blue, white, gold)');
+   			$('h2:first').hide();
    		}
 	});
 
@@ -71,9 +152,10 @@
  		//increments the number of resets
  		$('#num-resets').html(function(i, val) { return +val+1 });
  		//resets all shots and hits from both teams back to zero
- 		$('#teamone-numshots').html(function(i, val) { return 0});
- 		$('#teamone-numhits').html(function(i, val) { return 0});
- 		$('#teamtwo-numshots').html(function(i, val) { return 0});
- 		$('#teamtwo-numhits').html(function(i, val) { return 0});
- 		$('body').css('background-color', 'white');
+ 		$('#teamone-numshots').html(function(i, val) { return 0 });
+ 		$('#teamone-numhits').html(function(i, val) { return 0 });
+ 		$('#teamtwo-numshots').html(function(i, val) { return 0 });
+ 		$('#teamtwo-numhits').html(function(i, val) { return 0 });
+ 		$('body').css('background', 'linear-gradient(to right, blue, white, gold)');
+ 		$('h2:first').hide();
  	});
